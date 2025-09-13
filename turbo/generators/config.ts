e1ddfa9f785d31fafd54e13cc5b0bbebfcfa3b8f
Joel Solano <joelsolano@jsolano.de>
2025-09-14 00:14:11 +0200

    chore(config): update plugin template (#22)

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,5 +1,7 @@
 import type { PlopTypes } from "@turbo/gen";
-import pluginCfg from "./plugin/config";
+import pluginCfg from "./plugin/generator";
+
+export const PACKAGE_PREFIX = "@better-auth-extended/" as const;
 
 export default function generator(plop: PlopTypes.NodePlopAPI) {
 	plop.setGenerator("plugin", pluginCfg);
