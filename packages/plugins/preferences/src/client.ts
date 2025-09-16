468018ee442cdc82b9843dd1dbb9a79073c60484
Joel Solano <joelsolano@jsolano.de>
2025-09-16 13:17:37 +0200

    fix: client path methods

--- packages/plugins/preferences/src/client.ts
+++ packages/plugins/preferences/src/client.ts
@@ -4,6 +4,7 @@ import type {
 	PreferenceScopeAttributes,
 	PreferencesOptions,
 } from "./index";
+import { toPath } from "./utils";
 
 export const preferencesClient = <
 	Scopes extends {
@@ -28,9 +29,34 @@ export const preferencesClient = <
 				>,
 			},
 		}),
-		pathMethods: {
-			"/preferences/set-preference": "POST",
-			// TODO:
-		},
+		fetchPlugins: [
+			{
+				id: "preferences",
+				name: "preferences",
+				hooks: {
+					async onRequest(context) {
+						const urlPath = toPath(context.url);
+						const basePathRaw = toPath(context.baseURL ?? "/api/auth");
+						const basePath = basePathRaw.endsWith("/")
+							? basePathRaw.slice(0, -1)
+							: basePathRaw;
+						if (urlPath === `${basePath}/preferences/set-preference`) {
+							return {
+								...context,
+								method: "POST",
+							};
+						} else if (
+							urlPath.startsWith(`${basePath}/preferences/`) &&
+							urlPath.endsWith("/set")
+						) {
+							return {
+								...context,
+								method: "POST",
+							};
+						}
+					},
+				},
+			},
+		],
 	} satisfies BetterAuthClientPlugin;
 };
