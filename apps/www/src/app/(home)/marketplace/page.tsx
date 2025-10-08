1703f5bba0bf3e6398b92a76ec98a1f7485c7938
Joel Solano <joelsolano@jsolano.de>
2025-10-08 16:25:12 +0200

    chore: fix linting

--- apps/www/src/app/(home)/marketplace/page.tsx
+++ apps/www/src/app/(home)/marketplace/page.tsx
@@ -1,23 +1,10 @@
-import { buttonVariants } from "@/components/ui/button";
-import { ArrowRightIcon } from "lucide-react";
-import {
-	Card,
-	CardDescription,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import Link from "next/link";
 import { Marketplace } from "./client";
 import { createMetadata } from "@/lib/metadata";
-import { owner, repo } from "@/lib/github";
-import { Suspense } from "react";
 
-// TODO: Update description
 export const metadata = createMetadata({
 	title: "Marketplace",
 	description:
-		"Ea aliquip pariatur nisi amet voluptate minim occaecat dolor est laboris.",
+		"Discover and share free open-source resources and plugins for Better Auth.",
 	openGraph: {
 		url: "/marketplace",
 	},
